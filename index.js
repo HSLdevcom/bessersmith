96c00c1cbed4f4d80c3a3d64562e8116a01f3ac5
haphut <haphut@gmail.com>
2017-10-30 18:16:05 +0200

    Fix index.js to be accepted by ESLint

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const all = require('./src/all');
+const all = require("./src/all");
 
-httpServer = all.createServer();
-httpServer.listen(9009);
+const main = () => {
+  const httpServer = all.createServer();
+  httpServer.listen(9009);
+};
+
+if (require.main === module) {
+  main();
+}
