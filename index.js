a2c7964b18af6a56c1003ba9d6e3f6ed1ca7b601
haphut <haphut@gmail.com>
2017-11-03 19:38:03 +0200

    Rewrite index.js to use new code

--- index.js
+++ index.js
@@ -1,8 +1,52 @@
-const all = require("./src/all");
+const { createCache } = require("./src/cache");
+const { getFeedMessage } = require("./src/gtfsrt");
+const { createServer } = require("./src/http");
+const { startMQTTSubscription } = require("./src/mqtt");
+
+const run = async config => {
+  const cache = createCache(config.cache);
+  let feedMessage;
+  try {
+    feedMessage = await getFeedMessage(config.protoFilename);
+  } catch (err) {
+    // FIXME: log as error
+    console.log("Something weird happened in getFeedMessage:", err);
+    process.exit(1);
+  }
+  const server = createServer(cache, feedMessage);
+  startMQTTSubscription(config.mqtt, cache);
+  server.listen(config.http.listeningOptions, () => {
+    // FIXME: log with info
+    console.log("The HTTP server has been bound");
+  });
+};
 
 const main = () => {
-  const httpServer = all.createServer();
-  httpServer.listen(9009);
+  // FIXME: read config from config file
+  const config = {
+    mqtt: {
+      url: "mqtt://mqtt.hsl.fi",
+      connectionOptions: {
+        // FIXME: Fix broker settings.
+        // clean: false
+        clean: true
+      },
+      subscriptionOptions: {
+        qos: 1
+      },
+      topic: "mono/v2/predictions/#"
+    },
+    http: {
+      listeningOptions: {
+        port: 8087
+      }
+    },
+    cache: {
+      ttlInSeconds: 60 * 60 * 2
+    },
+    protoFilename: "gtfs-realtime.proto"
+  };
+  Promise.resolve(run(config));
 };
 
 if (require.main === module) {
